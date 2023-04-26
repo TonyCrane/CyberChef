f6326b97dcece813d0ed2a4ac6b368d5440f97e5
TonyCrane <tonycrane@foxmail.com>
2023-04-26 14:36:17 +0800

    Add RISC-V Encode labels support

--- src/core/operations/RISCVEncode.mjs
+++ src/core/operations/RISCVEncode.mjs
@@ -30,7 +30,9 @@ class RISCVEncode extends Operation {
             "<br/>",
             "Support RV32I, RV64I, RV128I with Zifencei, Zicsr, MAFDQC extensions",
             "<br/><br/>",
-            "<strong>Note: </strong>Can't use pseudo-instructions and labels now",
+            "<strong>Note: </strong>Can't use pseudo-instructions",
+            "<br/>",
+            "<strong>Note: </strong>Only support simple labels (not well-tested, unstable)",
         ].join("\n");
         this.infoURL = "https://riscv.org/technical/specifications/";
         this.inputType = "string";
@@ -84,27 +86,23 @@ class RISCVEncode extends Operation {
      */
     run(commands, args) {
         const [isa, outputMode, ignoreIllegal, showAddr, showInput] = args;
-        const insts = commands.split(/\n+/);
+        const [insts, refList, lineMap] = this.parseRef(commands);
+        const invRefList = this.invRef(refList);
         const instBytecode = [];
         const instBytes = [];
         const ret = [];
-        if (insts[0] === "") {
-            insts.shift();
-        }
-        if (insts[insts.length - 1] === "") {
-            insts.pop();
-        }
         const instNum = insts.length;
         const addrLength = Math.ceil(Math.log(instNum * 4) / Math.log(16));
         for (let i = 0; i < instNum; ++i) {
             let inst;
+            const instDeref = this.deref(insts[i], refList, lineMap[i], i * 4);
             try {
-                inst = new Instruction(insts[i], { ISA: isa });
+                inst = new Instruction(instDeref, { ISA: isa });
             } catch (error) {
                 if (ignoreIllegal) {
-                    inst = Object({hex: "ffffffff"});
+                    inst = {hex: "ffffffff", bin: "11111111111111111111111111111111"};
                 } else {
-                    throw new OperationError(`Error at line ${i + 1}: ${error.message}`);
+                    throw new OperationError(`Error at line ${lineMap[i]}: ${error.message}`);
                 }
             }
             instBytecode.push(inst.hex);
@@ -118,10 +116,16 @@ class RISCVEncode extends Operation {
                 }
                 if (showAddr && showInput) {
                     ret.push(`0x${(i * 4).toString(16).padStart(addrLength, "0")}: ${instStr} [ ${insts[i].trim()} ]`);
+                    if (invRefList[i * 4] !== undefined) {
+                        ret[ret.length - 1] += ` (label: ${invRefList[i * 4].join(", ")})`;
+                    }
                 } else if (showAddr) {
                     ret.push(`0x${(i * 4).toString(16).padStart(addrLength, "0")}: ${instStr}`);
                 } else if (showInput) {
                     ret.push(`${instStr} [ ${insts[i].trim()} ]`);
+                    if (invRefList[i * 4] !== undefined) {
+                        ret[ret.length - 1] += ` (label: ${invRefList[i * 4].join(", ")})`;
+                    }
                 } else {
                     ret.push(instStr);
                 }
@@ -132,6 +136,87 @@ class RISCVEncode extends Operation {
         } else {
             return Utils.strToByteArray(ret.join("\n"));
         }
+    }
+
+    /**
+     * @param {string} commands
+     * @returns {Array, Array}
+     */
+    parseRef(commands) {
+        const codes = commands.split(/\n+/);
+        if (codes[0] === "") {
+            codes.shift();
+        }
+        if (codes[codes.length - 1] === "") {
+            codes.pop();
+        }
+        const codeLength = codes.length;
+        const refList = {};
+        const insts = [];
+        const lineMap = {};
+        for (let i = 0; i < codeLength; ++i) {
+            if (!codes[i].includes(":")) {
+                lineMap[insts.length] = i + 1;
+                insts.push(codes[i].trim());
+                continue;
+            }
+            const refName = codes[i].trim().split(":")[0];
+            if (refList[refName] !== undefined) {
+                throw new OperationError(`Error at line ${i + 1}: Duplicate label name "${refName}"`);
+            }
+            if (/^\d/.test(refName)) {
+                throw new OperationError(`Error at line ${i + 1}: label name (${refName}) can't start with number`);
+            }
+            refList[refName] = insts.length * 4;
+            if (!codes[i].trim().endsWith(":")) {
+                lineMap[insts.length] = i + 1;
+                insts.push(codes[i].trim().split(":")[1].trim());
+            }
+        }
+        return [insts, refList, lineMap];
+    }
+
+    /**
+     * @param {string} inst
+     * @param {Array} refList
+     * @param {Number} lineNum
+     * @param {Number} instAddr
+     * @returns {string}
+     */
+    deref(inst, refList, lineNum, instAddr) {
+        const tokens = inst.split(/[ ,()]+/);
+        console.log(tokens);
+        const mneList = [
+            "jal",
+            "beq", "bne", "blt", "bge", "bltu", "bgeu",
+        ];
+        if (!mneList.includes(tokens[0].toLowerCase())) return inst;
+        const target = tokens[tokens.length - 1];
+        console.log(target);
+        if ((target.includes("0x") && !isNaN(parseInt(target, 16))) || !isNaN(parseInt(target, 10))) return inst;
+        if (refList[target] === undefined) {
+            throw new OperationError(`Error at line ${lineNum}: Undefined label "${target}"`);
+        }
+        const offset = refList[target] - instAddr;
+        tokens.pop();
+        return tokens.join(" ") + " " + offset;
+    }
+
+    /**
+     * @param {Array} refList
+     * @returns {Array}
+     */
+    invRef(refList) {
+        const invRefList = {};
+        for (const ref in refList) {
+            const dest = refList[ref];
+            if (invRefList[dest] === undefined) {
+                invRefList[dest] = [ref];
+            } else {
+                invRefList[dest].push(ref);
+            }
+        }
+        return invRefList;
     }
 
 }
